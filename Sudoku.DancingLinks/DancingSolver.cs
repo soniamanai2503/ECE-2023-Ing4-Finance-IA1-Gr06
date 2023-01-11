7bdc7010d2894a3d8f55fc25c88a95c58f4e3079
capucine5 <capucine.frossard@edu.ece.fr>
2023-01-11 11:50:58 +0100

    Dancing solver vide

---
+++ Sudoku.DancingLinks/DancingSolver.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.DancingLinks
+{
+    public class DancingSolver :ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s.CloneSudoku();
+    }
+
+}
+}
