995c91981edbf80affb794c8214c3907e008d86b
Paulanicetkouadio <nguettiagnanmtchepaulanicet.kouadio@edu.ece.fr>
2023-01-11 11:35:28 +0100

    Benchmark squelette python

---
+++ Sudoku.Backtracking/Backtrackingsolver1.cs
@@ -0,0 +1,38 @@
+﻿using Python.Runtime;
+using System.Resources;
+using Sudoku.Shared;
+
+namespace Sudoku.Backtracking
+{
+    public class Backtrackingsolver1 : PythonSolverBase
+
+    {
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+        {
+            //System.Diagnostics.Debugger.Break();
+
+            //For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pyCells = s.Cells.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("instance", pyCells);
+
+                // the person object may now be used in Python
+                string code = Resource1.Backtracking_py;
+                scope.Exec(code);
+                var result = scope.Get("r");
+                var managedResult = result.As<int[][]>();
+                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+                return new Shared.SudokuGrid() { Cells = managedResult };
+            }
+            //}
+
+        }
+    }
+    }
