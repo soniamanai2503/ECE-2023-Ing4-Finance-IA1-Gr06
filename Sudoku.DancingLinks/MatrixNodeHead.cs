c44e5e486ca4313f0f2488b135808514e4dc4c1b
Jean-Sylvain Boige <jsboige@gmail.com>
2023-02-05 14:57:49 +0100

    Corrections et fusion dlx

---
+++ Sudoku.DancingLinks/MatrixNodeHead.cs
@@ -0,0 +1,15 @@
+namespace Sudoku.DancingLinks
+{
+    class MatrixNodeHead : MatrixNode
+    {
+        internal string name;
+        internal new MatrixNode item;
+        internal int size;
+
+        public MatrixNodeHead(string s) : base(null)
+        {
+            name = s;
+        }
+
+    }
+}
