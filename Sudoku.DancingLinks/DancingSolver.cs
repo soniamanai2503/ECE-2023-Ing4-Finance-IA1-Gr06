cf587c2f025b4943adfe74fb4edd30ddc3d52471
soniamanai2503 <91455088+soniamanai2503@users.noreply.github.com>
2023-02-13 12:15:51 +0100

    Update DancingSolver.cs

--- Sudoku.DancingLinks/DancingSolver.cs
+++ Sudoku.DancingLinks/DancingSolver.cs
@@ -1,7 +1,17 @@
 using Sudoku.Shared;
 
 namespace Sudoku.DancingLinks;
-public class DlxLibSolver : ISudokuSolver
+public class DancingSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        MatrixList dlxList = new MatrixList(s.Cells);
+        dlxList.search();
+        s.Cells = dlxList.convertMatrixSudoku();
+        return s;
+    }
+}
+public class DlxSolver:ISudokuSolver
 {
     public SudokuGrid Solve(SudokuGrid s)
     {
